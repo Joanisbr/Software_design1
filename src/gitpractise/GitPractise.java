35e4efc0f381f32857da86859375f0f2967cfdf6
Joanisbr <Joanisbr@sheridancollege.ca>
2022-02-23 11:20:43 -0500

    Revert "added Systemoutput code "
    This reverts commit ec1b42a88a6e846d6ba69a88ac91472c60b9cd16.

--- src/gitpractise/GitPractise.java
+++ src/gitpractise/GitPractise.java
@@ -15,7 +15,6 @@ public class GitPractise {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hello world!");
     }
     
 }
