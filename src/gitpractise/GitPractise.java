ec1b42a88a6e846d6ba69a88ac91472c60b9cd16
Joanisbr <Joanisbr@sheridancollege.ca>
2022-02-23 11:20:19 -0500

    added Systemoutput code

--- src/gitpractise/GitPractise.java
+++ src/gitpractise/GitPractise.java
@@ -15,6 +15,7 @@ public class GitPractise {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello world!");
     }
     
 }
